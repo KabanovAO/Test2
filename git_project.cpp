93b038477ac2c5097c2147e419d964884730d71e
KabanovAO <Kabanov_andrew@mail.ru>
2021-05-23 18:15:40 +0300

    Andrey

--- git_project.cpp
+++ git_project.cpp
@@ -5,8 +5,8 @@ int main() {
 	//note comment
 	//2-nd comment
 
-	std::cout << "Hello Denis" << std::endl;
+	std::cout << "Hello, Denis" << std::endl;
 	std::cout << "hello, Andrey" << std::endl;
-
+	// Andrey comment
 	return 1;
 }

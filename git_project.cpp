6d71fd0cfe0933c13c1bf0c71b5f49e8e3a20a8b
KabanovAO <Kabanov_andrew@mail.ru>
2021-05-23 19:43:27 +0300

    Change 2

--- git_project.cpp
+++ git_project.cpp
@@ -5,7 +5,6 @@ int main()
 {
 	std::cout << sum1(5, 4);
 
-<<<<<<< HEAD
 	//note comment
 	//2-nd comment
 
@@ -13,7 +12,6 @@ int main()
 	std::cout << sum1(5, 3);
 	// Andrey comment
 	// Denis comment
-=======
->>>>>>> 60927a09b4c37a2c9b8e14bd98c5cd6030f60ace
+
 	return 1;
 }
